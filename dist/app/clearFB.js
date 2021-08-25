f564e16f99c6ea72321051f334704a03574f4efb
Duong Minh Chien <minhchienwikipedia@gmail.com>
2021-08-25 20:18:02 +0700

    Update

--- dist/app/clearFB.js
+++ dist/app/clearFB.js
@@ -1,7 +1,9 @@
 (function () {
-  document
-    .querySelectorAll(".j83agx80.cbu4d94t.buofh1pr")
-    .forEach(function (a) {
-      a.remove();
-    });
+  setTimeout(() => {
+    document
+      .querySelectorAll(".j83agx80.cbu4d94t.buofh1pr")
+      .forEach(function (a) {
+        a.remove();
+      });
+  }, 3000);
 })();
